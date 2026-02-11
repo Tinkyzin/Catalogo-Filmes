cbac13be9c2594a5eb516c2e272b68b0b450ca92
Breno Ornelas Rodrigues <brenoornelas125@gmail.com>
2026-02-10 21:46:27 -0300

    Update main.js

--- Catálogo/js/main.js
+++ Catálogo/js/main.js
@@ -1,6 +1,7 @@
 const API_KEY ="9ed4d53db23dc98cfda64348b807b218";
 const BASE_URL = "https://api.themoviedb.org/3";
 const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+
 const campoPesquisa = document.getElementById("campoPesquisa");
 const botaoPesquisa = document.getElementById("botaoPesquisa");
 const filmesGrid =document.getElementById("filmesGrid");
@@ -15,8 +16,10 @@ async function requisicao(url) {
         renderizarFilmes(data.results);
     } catch (error) {
         console.error("Erro:", error);
+        filmesGrid.innerHTML = "<p>Erro ao carregar filmes.</p>";
     }
 }
+
 function buscaFilme() {
     const informacao =  campoPesquisa.value.trim();
     if (informacao === "") {
@@ -24,8 +27,37 @@ function buscaFilme() {
         return;
     }
     console.log("pesquisando por:", informacao);
+    const url = `${BASE_URL}/search/movie?api_key=${API_KEY}
+    &query=${encodeURIComponent(informacao)}&language=pt-BR`;
+    requisicaoFilmes(url);
     campoPesquisa.value ="";
 }
+
+function renderizarFilmes (filmes) {
+    filmesGrid.innerHTML ="";
+    if (!filmes || filmes.length === 0) {
+        filmesGrid.innerHTML = "<p>Nenhum filme encontrado.</p>";
+        return;
+    }
+    filmes.forEach(filme => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        const imagem= filme.poster_path
+            ? IMAGE_URL + filme.poster_path
+            : "";
+        card.innerHTML= `
+            <img src="${imagem}" alt="${filme.title}"
+            <h3>${filme.title}</h3>
+            <p>${filme.vote_average}
+                ${filme.overview}
+                ${filme.genre_ids}
+                ${filme.release_date}
+            </p>
+        `;
+        filmesGrid.appendChild(card);
+    });
+}
+
 botaoPesquisa.addEventListener("click", buscaFilme);
 campoPesquisa.addEventListener("keydown",function (event) {
     if (event.key === "Enter") {
