1dfe684a249016f89462f5a4ea66447dadb1e390
Breno Ornelas Rodrigues <brenoornelas125@gmail.com>
2026-02-24 22:13:54 -0300

    Replace JavaScript code with HTML structure

--- Catálogo/js/main.js
+++ Catálogo/js/main.js
@@ -1,65 +1,26 @@
-const API_KEY ="a3fda9b9d1d0aaee95df37313c16684e";
-const BASE_URL = "https://api.themoviedb.org/3";
-const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
-
-const campoPesquisa = document.getElementById("campoPesquisa");
-const botaoPesquisa = document.getElementById("botaoPesquisa");
-const filmesGrid =document.getElementById("filmesGrid");
-
-async function requisicaoFilmes(url) {
-    try{
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error("Erro na requisição");
-        }
-        const data = await response.json()
-        renderizarFilmes(data.results);
-    } catch (error) {
-        console.error("Erro:", error);
-        filmesGrid.innerHTML = "<p>Erro ao carregar filmes.</p>";
-    }
-}
-
-function buscaFilme() {
-    const informacao =  campoPesquisa.value.trim();
-    if (informacao === "") {
-        window.location.reload();
-        return;
-    }
-    console.log("pesquisando por:", informacao);
-    const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(informacao)}&language=pt-BR`;
-    requisicaoFilmes(url);
-    campoPesquisa.value ="";
-}
-
-function renderizarFilmes (filmes) {
-    filmesGrid.innerHTML ="";
-    if (!filmes || filmes.length === 0) {
-        filmesGrid.innerHTML = "<p>Nenhum filme encontrado.</p>";
-        return;
-    }
-    filmes.forEach(filme => {
-        const card = document.createElement("div");
-        card.classList.add("card");
-        const imagem= filme.poster_path
-            ? IMAGE_URL + filme.poster_path
-            : "";
-        card.innerHTML= `
-            <img src="${imagem}" alt="${filme.title}"
-            <h3>${filme.title}</h3>
-            <p>${filme.vote_average}
-                ${filme.overview}
-                ${filme.genre_ids}
-                ${filme.release_date}
-            </p>
-        `;
-        filmesGrid.appendChild(card);
-    });
-}
-
-botaoPesquisa.addEventListener("click", buscaFilme);
-campoPesquisa.addEventListener("keydown",function (event) {
-    if (event.key === "Enter") {
-        buscaFilme();
-    }
-});
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detalhe</title>
+</head>
+<body>
+    <header class="header">
+        <div class="conteiner header-conteudo">
+            <h1 class="logo"><img src="assets/ícones/Logo.jpeg" alt="Logo" id="logo1"></h1>
+            <nav class="menu">
+                <a href="#" id="inicio">Início</a>
+                <a href="#">Filmes</a>
+                <a href="#">Séries</a>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <div class="detalhesConteiner" id="detalhesConteiner"></div>
+    </main>
+    <footer class="footer">
+        <p>Desenvolvido por Breno © 2026</p>
+    </footer>
+</body>
+</html>
